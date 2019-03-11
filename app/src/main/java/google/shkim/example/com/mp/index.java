41848e34d0c84e035566f335ab9c9a3c40f81add
sung_hyun_Kim <shkim787@naver.com>
2019-03-11 13:25:33 +0900

    다시 초기화

--- app/src/main/java/google/shkim/example/com/mp/index.java
+++ app/src/main/java/google/shkim/example/com/mp/index.java
@@ -1,37 +1,18 @@
 package google.shkim.example.com.mp;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class index extends AppCompatActivity {
+public class index extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
-        Button button1 = (Button)findViewById(R.id.button1);
-        Button button2 = (Button)findViewById(R.id.button2);
-        Button button3 = (Button)findViewById(R.id.button3);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"1번 버튼을 눌렀어요",Toast.LENGTH_SHORT).show();
-            }
-        });
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"2번 버튼을 눌렀어요",Toast.LENGTH_SHORT).show();
-            }
-        });
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"3번 버튼을 눌렀어요",Toast.LENGTH_SHORT).show();
-            }
-        });
+
     }
 }
