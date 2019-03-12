62d27d4f47d8cf9b1e74942c937225759a9b2eb9
sung_hyun_Kim <shkim787@naver.com>
2019-03-12 18:21:04 +0900

    검색창 적용

--- app/src/main/java/google/shkim/example/com/mp/google.java
+++ app/src/main/java/google/shkim/example/com/mp/google.java
@@ -2,45 +2,73 @@ package google.shkim.example.com.mp;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+
 
 public class google extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    PlaceAutocompleteFragment placeAutoComplete;
+    private static final String DEBUG_TAG = "{LOG_ANDROID}";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        initViews();
     }
 
+    private void initViews() {
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete);
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+        Log.d(DEBUG_TAG, "a");
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+
+                // TODO: Get info about the selected place.
+
+                final LatLng location = place.getLatLng();
+                Log.d(DEBUG_TAG, "name : " + place.getName());
+                Log.d(DEBUG_TAG, "address : " + place.getAddress());
+                Log.d(DEBUG_TAG, "id : " + place.getId());
+                Log.d(DEBUG_TAG, "location : " + location.latitude + ", " + location.longitude);
+                //세부정보 가져오기
+
+
+                mMap.animateCamera(CameraUpdateFactory.newLatLng(location));
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 15));
+
+
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i("LOG", "An error occurred: " + status);
+            }
+        });
+    }  //검색바에 장소 정보 적용
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
