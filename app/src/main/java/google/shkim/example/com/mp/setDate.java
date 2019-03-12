6b1023e595b0c7ade72b8d829ad0658418ffe72b
Madlar <pjcj1@naver.com>
2019-03-12 22:44:30 +0900

    일정 추가 4차 수정(갤럭시 캘린더 화면 띄우기 까지 mp 지움)

--- app/src/main/java/google/shkim/example/com/mp/setDate.java
+++ app/src/main/java/google/shkim/example/com/mp/setDate.java
@@ -1,9 +1,10 @@
 package google.shkim.example.com.mp;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 
-public class setDate extends AppCompatActivity {
+
+public class setDate extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
