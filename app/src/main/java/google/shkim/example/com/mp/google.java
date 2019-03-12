c4bd908893228e6f0b539d2f1ccd8730a603de25
sung_hyun_Kim <shkim787@naver.com>
2019-03-12 22:22:24 +0900

    db생성, 좌표값 넣기 시도중

--- app/src/main/java/google/shkim/example/com/mp/google.java
+++ app/src/main/java/google/shkim/example/com/mp/google.java
@@ -1,8 +1,12 @@
 package google.shkim.example.com.mp;
 
+import android.content.Intent;
+import android.graphics.Point;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.Place;
@@ -12,6 +16,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -21,6 +26,7 @@ public class google extends FragmentActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
     PlaceAutocompleteFragment placeAutoComplete;
     private static final String DEBUG_TAG = "{LOG_ANDROID}";
+    private Database dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,7 +80,43 @@ public class google extends FragmentActivity implements OnMapReadyCallback {
     }  //검색바에 장소 정보 적용
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(GoogleMap googleMap)
+    {
         mMap = googleMap;
+        Button okbtn = (Button)findViewById(R.id.saveBtn);
+        Button calcelbtn = (Button)findViewById(R.id.cancelBtn);
+        dbHelper = new Database(getApplicationContext(), "Marker.db", null, 1);
+        okbtn.setOnClickListener(new View.OnClickListener() //확인 버튼 클릭 이벤트
+        {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(google.this, index.class);
+                startActivity(intent);
+                dbHelper.insert("insert into markerPoint(PlaceName, Lat, Lng) values('"+ place.getName() +"',"+ latLng.latitude + ", " + latLng.longitude + ");");
+            }
+        });
+        calcelbtn.setOnClickListener(new View.OnClickListener() //취소 버튼 클릭 이벤트
+        {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(google.this, index.class);
+                startActivity(intent);
+            }
+        });
+
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() //꾹 누르고 있을때 마커 찍히는 이벤트
+        {
+            @Override
+            public void onMapLongClick(final LatLng latLng)
+            {
+                MarkerOptions markerOptions = new MarkerOptions();
+                mMap.addMarker(new MarkerOptions().position(latLng));
+                    markerOptions.position(latLng).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)); //마커위치설정
+                    mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));   // 마커생성위치로 이동
+                    mMap.addMarker(markerOptions); //마커 생성
+
+            }
+
+        });
     }
 }
