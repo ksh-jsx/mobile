d511abd40f0771984c92d4eeac80582e84d419ab
sung_hyun_Kim <shkim787@naver.com>
2019-03-12 14:25:41 +0900

    aa

--- app/src/main/java/google/shkim/example/com/mp/index.java
+++ app/src/main/java/google/shkim/example/com/mp/index.java
@@ -5,7 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
+import com.skt.Tmap.TMapView;
 
 public class index extends Activity {
 
@@ -14,5 +17,18 @@ public class index extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
 
+        RelativeLayout relativeLayout = new RelativeLayout(this);
+        TMapView tMapView = new TMapView(this);
+
+        tMapView.setSKTMapApiKey( "34382003-0b4e-4cfc-846d-4bcef9b6d7ce" );
+        tMapView.setCompassMode(true);
+        tMapView.setIconVisibility(true);
+        tMapView.setZoom(15);
+        tMapView.setMapType(TMapView.MAPTYPE_STANDARD);
+        tMapView.setLanguage(TMapView.LANGUAGE_KOREAN);
+        tMapView.setTrackingMode(true);
+        tMapView.setSightVisible(true);
+        relativeLayout.addView(tMapView);
+        setContentView(relativeLayout);
     }
 }
