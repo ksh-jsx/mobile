316e95ab75c51ae01b09ab0027edebf3ef2c45d5
sung_hyun_Kim <shkim787@naver.com>
2019-03-12 14:49:48 +0900

    google api 적용

--- app/src/main/java/google/shkim/example/com/mp/index.java
+++ app/src/main/java/google/shkim/example/com/mp/index.java
@@ -1,6 +1,7 @@
 package google.shkim.example.com.mp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +14,16 @@ public class index extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
+        Button gotoGoogle = (Button)findViewById(R.id.gotoGoogle);
 
+        gotoGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(index.this, google.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
