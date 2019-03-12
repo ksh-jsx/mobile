5ada8c88ff1a5ba88f713b37216a38d777daa938
Madlar <pjcj1@naver.com>
2019-03-12 22:35:19 +0900

    일정 추가 3차 수정(갤럭시 캘린더 화면 띄우기 까지)

--- app/src/main/java/google/shkim/example/com/mp/setDate.java
+++ app/src/main/java/google/shkim/example/com/mp/setDate.java
@@ -1,7 +1,12 @@
 package google.shkim.example.com.mp;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class setDate extends AppCompatActivity {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_date);
+    }
 }
