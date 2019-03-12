2b361774252b553f5f936c9f8356763369d66e56
Madlar <pjcj1@naver.com>
2019-03-12 22:22:40 +0900

    일정 추가 2차 수정 (갤럭시 캘린더)

--- app/src/main/java/google/shkim/example/com/mp/EditPlan.java
+++ app/src/main/java/google/shkim/example/com/mp/EditPlan.java
@@ -1,7 +1,10 @@
 package google.shkim.example.com.mp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EditPlan extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class EditPlan extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_plan);
+        Button setDate=(Button)findViewById(R.id.setDate);
+        Button setPositionButton=(Button)findViewById(R.id.setPositionButtion);
+        Button submit=(Button)findViewById(R.id.submit);
+
+        setDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EditPlan.this,setDate.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/google/shkim/example/com/mp/setDate.java
@@ -0,0 +1,7 @@
+package google.shkim.example.com.mp;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class setDate extends AppCompatActivity {
+
+}
