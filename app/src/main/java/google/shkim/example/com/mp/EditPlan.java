168263cc11d930bf529ec816a1d4a3f80042b9dc
Madlar <pjcj1@naver.com>
2019-03-17 23:58:52 +0900

    날짜 변경 가능 setDate.xml setDate.java 삭제

--- app/src/main/java/google/shkim/example/com/mp/EditPlan.java
+++ app/src/main/java/google/shkim/example/com/mp/EditPlan.java
@@ -1,25 +1,37 @@
 package google.shkim.example.com.mp;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class EditPlan extends Activity {
 
     private Database dbHelper;
-    long now = System.currentTimeMillis();
-    Date date = new Date(now);
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+    private int year= Calendar.getInstance().get(Calendar.YEAR);
+    private int month=Calendar.getInstance().get(Calendar.MONTH)+1;
+    private int date=Calendar.getInstance().get(Calendar.DATE);
 
-    TextView dateShow;
+    private DatePickerDialog.OnDateSetListener dateSetListener=new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int Year, int monthOfYear, int dayOfMonth) {
+            year=Year;
+            month=monthOfYear+1;
+            date=dayOfMonth;
+            TextView dateShow=(TextView)findViewById(R.id.dateShow);
+            dateShow.setText(year+" - "+month+" - "+date);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +43,9 @@ public class EditPlan extends Activity {
         Button back=(Button)findViewById(R.id.back);
         Button setLatLng = (Button)findViewById(R.id.setLatLng);
         TextView setaddress = (TextView)findViewById(R.id.setAddress);
+        TextView dateShow=(TextView)findViewById(R.id.dateShow);
 
-        dateShow=(TextView)findViewById(R.id.dateShow);
-        dateShow.setText(getTime());
+        dateShow.setText(year+" - "+month+" - "+date);
 
 
         TextView setlat = (TextView)findViewById(R.id.setPositionLatitude);
@@ -42,8 +54,7 @@ public class EditPlan extends Activity {
         setDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(EditPlan.this,setDate.class);
-                startActivity(intent);
+                new DatePickerDialog(EditPlan.this,dateSetListener,year,month-1,date).show();
             }
         });
         setLatLng.setOnClickListener(new View.OnClickListener() {
@@ -79,9 +90,5 @@ public class EditPlan extends Activity {
             setlng.setText(cursor1.getString(2));
         }
 
-    }
-
-    private String getTime() {
-        return sdf.format(date);
     }
 }
