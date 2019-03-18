b5c540dc7caa8e45fc39863782947429565f83cb
sung_hyun_Kim <shkim787@naver.com>
2019-03-18 15:37:16 +0900

    해야될 거 주석으로 적어둠

--- app/src/main/java/google/shkim/example/com/mp/EditPlan.java
+++ app/src/main/java/google/shkim/example/com/mp/EditPlan.java
@@ -98,8 +98,8 @@ public class EditPlan extends Activity {
                 Log.d(DEBUG_TAG, "year : " + yearValue);
                 Log.d(DEBUG_TAG, "month : " + monthValue);
                 Log.d(DEBUG_TAG, "date : " + hourValue);
-                Log.d(DEBUG_TAG, "hour : " + minuteValue);
-                Log.d(DEBUG_TAG, "minute : " + dateValue);
+                Log.d(DEBUG_TAG, "hour : " + minuteValue);//여기서 시간(시) 잘 뜨는지 확인가능함. 밑에 Logcat누르고 Verbose->Debug 선택하면 볼 수 있음
+                Log.d(DEBUG_TAG, "minute : " + dateValue);//여기서 시간(분) 잘 뜨는지 확인가능함. 밑에 Logcat누르고 Verbose->Debug 선택하면 볼 수 있음
                 Log.d(DEBUG_TAG, "location : " + latString + ", " + lngString);
                 if(nameValue.length() == 0)
                     Toast.makeText(getApplicationContext(), "장소명을 입력해주세요", Toast.LENGTH_SHORT).show();
