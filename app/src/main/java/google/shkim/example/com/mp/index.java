15b90e6ea2c29f3fa6ca62b18c2597cf7feab248
sung_hyun_Kim <shkim787@naver.com>
2019-04-02 17:29:43 +0900

    임시 저장

--- app/src/main/java/google/shkim/example/com/mp/index.java
+++ app/src/main/java/google/shkim/example/com/mp/index.java
@@ -21,6 +21,7 @@ import java.util.HashMap;
 
 public class index extends Activity {
 
+    private View header;
     private final long FINISH_INTERVAL_TIME = 2000;
     private long   backPressedTime = 0;
     private static final String DEBUG_TAG = "{LOG_ANDROID}";
@@ -36,6 +37,7 @@ public class index extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
         Button gotoPlan = (Button)findViewById(R.id.gotoPlan);
+        Button del = (Button)findViewById(R.id.del);
         final Database dbHelper = new Database(getApplicationContext(), "SQLite3.db", null, 1);
         ListView list = (ListView) findViewById(R.id.List_view);
         infoList = new ArrayList<HashMap<String,String>>();
@@ -48,46 +50,57 @@ public class index extends Activity {
         cursor1.moveToFirst();
         for(i=0;i<cursor1.getCount();i++)
         {
-            Log.d(DEBUG_TAG, "name : " + cursor1.getString(1));
+
+          Log.d(DEBUG_TAG, "name : " + cursor1.getString(1));
             Log.d(DEBUG_TAG, "time : " + cursor1.getString(5)+"시"+cursor1.getString(6)+"분");
             Log.d(DEBUG_TAG, "datesave : " + dateSave);
-
 
             name = cursor1.getString(1);
             date = cursor1.getString(2)+"년 "+cursor1.getString(3)+"월 "+cursor1.getString(4)+"일";
             time = cursor1.getString(5)+"시"+cursor1.getString(6)+"분";
             Log.d(DEBUG_TAG, "date : " + date);
 
+            header = getLayoutInflater().inflate(R.layout.activity_list_item, null, false);
+            LinearLayout nameTIme = (LinearLayout)header.findViewById(R.id.nameTime);
+            TextView dateView = (TextView)header.findViewById(R.id.date);
             HashMap<String,String> infos = new HashMap<String,String>();
             if(dateSave.equals(date))
             {
                 infos.put(TAG_NAME,name);
                 infos.put(TAG_TIME,time);
+                dateView.setVisibility(View.GONE);
+                nameTIme.setVisibility(View.VISIBLE);
                 cursor1.moveToNext();
             }
             else
             {
                 infos.put(TAG_DATE,date);
+                nameTIme.setVisibility(View.GONE);
+                dateView.setVisibility(View.VISIBLE);
                 i--;
             }
 
             //ArrayList에 추가합니다..
             infoList.add(infos);
-
             dateSave = date;
 
+            infoList.add(header);
+            if(date.equals("a")){}
 
+
+            adapter = new SimpleAdapter(
+                    this, infoList, R.layout.activity_list_item,
+                    new String[]{TAG_NAME,TAG_DATE,TAG_TIME},
+                    new int[]{ R.id.name, R.id.date, R.id.time}
+            );
+            list.setAdapter(adapter);
         }
 
-        adapter = new SimpleAdapter(
-                this, infoList, R.layout.list_item,
-                new String[]{TAG_NAME,TAG_DATE,TAG_TIME},
-                new int[]{ R.id.name, R.id.date, R.id.time}
-        );
+
 
 
         //화면에 보여주기 위해 Listview에 연결합니다.
-        list.setAdapter(adapter);
+
 
 
         //setContentView(layout);
@@ -99,6 +112,14 @@ public class index extends Activity {
                 dbHelper.delete("delete from tempSave;");
                 Intent intent = new Intent(index.this, EditPlan.class);
                 startActivity(intent);
+            }
+        });
+
+        del.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dbHelper.delete("delete from infos;");
+                view.invalidate();
             }
         });
 
