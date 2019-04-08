8e6670525e24ee1ec9a19b68d4bfd20b1d36680b
sung_hyun_Kim <shkim787@naver.com>
2019-04-08 11:45:05 +0900

    과제5 임시

--- app/src/main/java/google/shkim/example/com/mp/index.java
+++ app/src/main/java/google/shkim/example/com/mp/index.java
@@ -1,20 +1,15 @@
+
 package google.shkim.example.com.mp;
 
-import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.ImageView;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import android.widget.EditText;
 
 public class index extends AppCompatActivity {
 
@@ -22,10 +17,39 @@ public class index extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
+        final EditText editUrl = (EditText)findViewById(R.id.editUrl);
+        final  Button btnGo = (Button)findViewById(R.id.btnGo);
+        final  Button btnBack = (Button)findViewById(R.id.btnBack);
+        final  WebView web = (WebView)findViewById(R.id.webView1);
+
+        web.setWebViewClient(new CookWebViewCLient());
+
+        WebSettings webSet = web.getSettings();
+        webSet.setBuiltInZoomControls(true);
+
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                web.loadUrl(editUrl.getText().toString());
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                web.goBack();
+            }
+        });
+    }
+    class CookWebViewCLient extends WebViewClient
+    {
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            return super.shouldOverrideUrlLoading(view, request);
+        }
 
     }
 }
-
-
 
 
