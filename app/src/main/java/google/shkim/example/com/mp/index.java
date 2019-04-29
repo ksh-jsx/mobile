32335a162f6864c285e3c7f559759ddb1188ffae
sung_hyun_Kim <shkim787@naver.com>
2019-04-29 15:12:59 +0900

    탭호스트 구현

--- app/src/main/java/google/shkim/example/com/mp/index.java
+++ app/src/main/java/google/shkim/example/com/mp/index.java
@@ -5,13 +5,17 @@ import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -37,6 +41,60 @@ public class index  extends Activity
         final Database dbHelper = new Database(getApplicationContext(), "SQLite3.db", null, 1);
         ListView listview = (ListView)findViewById(R.id.List_view);
         TextView noticeText = (TextView)findViewById(R.id.notice);
+
+        ImageView tabWidget0 = new ImageView(this);
+        tabWidget0.setImageResource(R.drawable.tab_00);
+        ImageView tabWidget1 = new ImageView(this);
+        tabWidget1.setImageResource(R.drawable.tab_01);
+        ImageView tabWidget2 = new ImageView(this);
+        tabWidget2.setImageResource(R.drawable.tab_02);
+
+
+        final TabHost tabhost = (TabHost)findViewById(R.id.tabHost1);
+        tabhost.setup();
+
+        TabHost.TabSpec ts1 = tabhost.newTabSpec("Tab Spec 1") ;
+        ts1.setContent(R.id.content1) ;
+        ts1.setIndicator(tabWidget0);
+        tabhost.addTab(ts1)  ;
+
+        // 두 번째 Tab. (탭 표시 텍스트:"TAB 2"), (페이지 뷰:"content2")
+        final TabHost.TabSpec ts2 = tabhost.newTabSpec("Tab Spec 2") ;
+        ts2.setContent(R.id.content2) ;
+        ts2.setIndicator(tabWidget1);
+        tabhost.addTab(ts2) ;
+
+        // 세 번째 Tab. (탭 표시 텍스트:"TAB 3"), (페이지 뷰:"content3")
+        TabHost.TabSpec ts3 = tabhost.newTabSpec("Tab Spec 3") ;
+        ts3.setContent(R.id.content3) ;
+        ts3.setIndicator(tabWidget2);
+        tabhost.addTab(ts3) ;
+
+        tabhost.setCurrentTab(1);
+
+        for (int i = 0; i < tabhost.getTabWidget().getChildCount(); i++) {
+
+            tabhost.getTabWidget().getChildAt(i).setBackgroundColor(Color.parseColor("#FFFFFF"));
+            tabhost.getTabWidget().setStripEnabled(false);
+
+        }
+        tabhost.getTabWidget().getChildAt(1).setBackgroundColor(Color.parseColor("#2196F3"));
+        tabhost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String s) {
+
+                for (int i = 0; i < tabhost.getTabWidget().getChildCount(); i++) {
+                    tabhost.getTabWidget().getChildAt(i).setBackgroundColor(Color.parseColor("#FFFFFF"));
+                }
+                tabhost.getTabWidget().getChildTabViewAt(tabhost.getCurrentTab()).setBackgroundColor(Color.parseColor("#2196F3"));
+
+                if(tabhost.getCurrentTab() == 0)
+                {
+                    Log.d(DEBUG_TAG, "hello");
+                }
+            }
+        });
+
         // 데이터 생성.
         Cursor cursor1 = dbHelper.select("SELECT * FROM infos");
         cursor1.moveToFirst();
