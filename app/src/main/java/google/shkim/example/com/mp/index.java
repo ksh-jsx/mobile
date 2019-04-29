4e65e63c54914215355755175e120207588bc0ac
sung_hyun_Kim <shkim787@naver.com>
2019-04-29 11:13:04 +0900

    과제 7

--- app/src/main/java/google/shkim/example/com/mp/index.java
+++ app/src/main/java/google/shkim/example/com/mp/index.java
@@ -1,6 +1,7 @@
 
 package google.shkim.example.com.mp;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -15,88 +16,80 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class index extends AppCompatActivity {
-    LinearLayout baseLayout;
-    Button button1;
-    View toastView;
-    TextView textView3;
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.menu1, menu);
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
 
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        switch (item.getItemId())
-        {
-            case R.id.itemRed:
-                baseLayout.setBackgroundColor(Color.RED);
-                return true;
-            case R.id.itemGreen:
-                baseLayout.setBackgroundColor(Color.GREEN);
-                return true;
-            case R.id.itemBlue:
-                baseLayout.setBackgroundColor(Color.BLUE);
-                return true;
-        }
-        return false;
-    }
+public class index extends AppCompatActivity {
+    String fileName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
-        setTitle("여섯번째 과제");
+        setTitle("17101197");
+        final EditText tv = (EditText) findViewById(R.id.setText);
+        final Button btn = (Button)findViewById(R.id.btn1);
+        DatePicker dp = (DatePicker)findViewById(R.id.getDate);
 
-        baseLayout = (LinearLayout)findViewById(R.id.base);
-        button1 = (Button)findViewById(R.id.button1);
-        final TextView textView1= (TextView)findViewById(R.id.textView1);
-        final TextView textView2= (TextView)findViewById(R.id.textView2);
-        button1.setOnClickListener(new View.OnClickListener() {
+
+        Calendar cal = Calendar.getInstance();
+        int cYear = cal.get(Calendar.YEAR);
+        int cMonth = cal.get(Calendar.MONTH);
+        int cDay = cal.get(Calendar.DAY_OF_MONTH);
+        dp.init(cYear, cMonth, cDay, new DatePicker.OnDateChangedListener() {
             @Override
-            public void onClick(View view) {
-                int index=0;
-                final String[] versionArray = new String[] {"파이리","꼬부기","이상해씨"};
-                if(versionArray[0].equals(textView1.getText().toString()))
-                    index = 0;
-                else if(versionArray[1].equals(textView1.getText().toString()))
-                    index = 1;
-                else if(versionArray[1].equals(textView1.getText().toString()))
-                    index =2;
-
-                AlertDialog.Builder dlg = new AlertDialog.Builder(index.this);
-                dlg.setTitle("포켓몬");
-                dlg.setIcon(R.drawable.ball);
-                dlg.setSingleChoiceItems(versionArray,index,new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        textView2.setText(versionArray[i]);
-                    }
-                });
-                dlg.setNegativeButton("취소",null);
-                dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        textView1.setText(textView2.getText().toString());
-                        Toast toast = new Toast(index.this);
-                        toastView = (View)View.inflate(index.this,R.layout.toast1,null);
-                        textView3 = (TextView)toastView.findViewById(R.id.toastText1);
-                        textView3.setText(textView2.getText().toString()+"를 선택했습니다");
-                        toast.setView(toastView);
-                        toast.show();
-                    }
-                });
-                dlg.show();
+            public void onDateChanged(DatePicker datePicker, int i, int i1, int i2) {
+                fileName = Integer.toString(i) +"_" + Integer.toString(i1) +"_"
+                        + Integer.toString(i2) + ".txt";
+                String str = readDairy(fileName);
+                tv.setText(str);
+                btn.setEnabled(true);
             }
         });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try
+                {
+                    FileOutputStream outFs = openFileOutput(fileName, Context.MODE_PRIVATE);
+                    String str = tv.getText().toString();
+                    outFs.write(str.getBytes());
+                    outFs.close();
+                    Toast.makeText(getApplicationContext(),fileName+"이 저장됨",Toast.LENGTH_SHORT).show();
+                }
+                catch(IOException e){}
+            }
+        });
+
+    }
+    String readDairy(String fName)
+    {
+        EditText tv = (EditText) findViewById(R.id.setText);
+        Button btn = (Button)findViewById(R.id.btn1);
+        String diaryStr = null;
+        FileInputStream infs;
+        try{
+            infs = openFileInput(fName);
+            byte[] txt = new byte[500];
+            infs.read(txt);
+            infs.close();
+            diaryStr = (new String(txt).trim());
+            btn.setText("수정하기");
+        }catch(IOException e){
+            tv.setHint("일기없음");
+            btn.setText("새로저장");
+        }
+        return  diaryStr;
     }
 }
 
