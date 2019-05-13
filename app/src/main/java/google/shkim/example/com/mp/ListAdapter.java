53a4dfb0250bc46b82258ae68f0079eb64ad6e89
sung_hyun_Kim <shkim787@naver.com>
2019-05-13 15:59:36 +0900

    리스트뷰 무너지는 현상 해결

--- app/src/main/java/google/shkim/example/com/mp/ListAdapter.java
+++ app/src/main/java/google/shkim/example/com/mp/ListAdapter.java
@@ -1,6 +1,7 @@
 package google.shkim.example.com.mp;
 
 import android.content.Context;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -52,36 +53,64 @@ public class ListAdapter extends BaseAdapter
     @Override
     public View getView(int position, View convertView, ViewGroup parent)
     {
-
+        final Context context = parent.getContext();
+        final CustomViewHolder holder;
+        View v = convertView;
         if(position ==0)
             dateSave ="a";
 
-        CustomViewHolder holder;
-        if (convertView == null)
+
+        if (v == null)
         {
-            final Context context = parent.getContext();
-            if (inflater == null)
-            {
-                inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            }
-            convertView = inflater.inflate(R.layout.list_item, parent, false);
+
+
+            inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = inflater.inflate(R.layout.list_item, parent, false);
+
             holder = new CustomViewHolder();
+            holder.oTextTitle = (TextView) v.findViewById(R.id.textTitle);
+            holder.oTextTIme = (TextView) v.findViewById(R.id.textTime);
+            holder.oTextDate = (TextView) v.findViewById(R.id.textDate);
+            holder.oTextLat = (TextView) v.findViewById(R.id.textLat);
+            holder.oTextLng = (TextView) v.findViewById(R.id.textLng);
+            holder.oimgName = (TextView)v.findViewById(R.id.imgName);
+            holder.dateLinear = (LinearLayout)v.findViewById(R.id.dateLinear);
+            holder.etc = (LinearLayout)v.findViewById(R.id.etc);
+            holder.lineImage = (ImageView)v.findViewById(R.id.lineImage);
 
-            holder.oTextTitle = (TextView) convertView.findViewById(R.id.textTitle);
-            holder.oTextTIme = (TextView) convertView.findViewById(R.id.textTime);
-            holder.oTextDate = (TextView) convertView.findViewById(R.id.textDate);
-            holder.oTextLat = (TextView) convertView.findViewById(R.id.textLat);
-            holder.oTextLng = (TextView) convertView.findViewById(R.id.textLng);
-            holder.oimgName = (TextView)convertView.findViewById(R.id.imgName);
-            holder.dateLinear = (LinearLayout)convertView.findViewById(R.id.dateLinear);
-            holder.etc = (LinearLayout)convertView.findViewById(R.id.etc);
-            holder.lineImage = (ImageView)convertView.findViewById(R.id.lineImage);
+            if (dateSave.equals(m_oData.get(position).strDate))
+            {
+                holder.dateLinear.setVisibility(View.GONE);
+                holder.etc.setVisibility(View.VISIBLE);
 
-            convertView.setTag(holder);
+                Log.d(DEBUG_TAG, "count1: " + m_oData.get(position).count1);
+                Log.d(DEBUG_TAG, "count2: " + m_oData.get(position).count2);
+                if (m_oData.get(position).count1 == m_oData.get(position).count2) {
+                    holder.lineImage.setImageResource(R.drawable.line_blue_non_bottom);
+                    holder.oimgName.setText("last");
+                }
+            }
+            else
+            {
+                holder.dateLinear.setVisibility(View.VISIBLE);
+                holder.etc.setVisibility(View.GONE);
+            }
+
+            holder.oTextTitle.setText(m_oData.get(position).strTitle);
+            holder.oTextTitle.setTag(position);
+            holder.oTextTIme.setText(m_oData.get(position).strTIme);
+            holder.oTextDate.setText(m_oData.get(position).strDate);
+            holder.oTextLat.setText(m_oData.get(position).strLat);
+            holder.oTextLng.setText(m_oData.get(position).strLng);
+            dateSave = m_oData.get(position).strDate;
+
+            v.setTag(holder);
+            Log.d(DEBUG_TAG, "hello 1"+v.getTag());
         }
-        else
-            holder = (CustomViewHolder) convertView.getTag();
-
+        else {
+            holder = (CustomViewHolder) v.getTag();
+            Log.d(DEBUG_TAG, "hello 2"+holder);
+        }
         /*TextView oTextTitle = (TextView) convertView.findViewById(R.id.textTitle);
         TextView oTextTIme = (TextView) convertView.findViewById(R.id.textTime);
         TextView oTextDate = (TextView) convertView.findViewById(R.id.textDate);
@@ -92,36 +121,12 @@ public class ListAdapter extends BaseAdapter
         LinearLayout etc = (LinearLayout)convertView.findViewById(R.id.etc);
         ImageView lineImage = (ImageView)convertView.findViewById(R.id.lineImage);*/
 
-        if (dateSave.equals(m_oData.get(position).strDate))
-        {
-            holder.dateLinear.setVisibility(View.GONE);
-            holder.etc.setVisibility(View.VISIBLE);
 
-            Log.d(DEBUG_TAG, "count1: " + m_oData.get(position).count1);
-            Log.d(DEBUG_TAG, "count2: " + m_oData.get(position).count2);
-            if (m_oData.get(position).count1 == m_oData.get(position).count2) {
-                holder.lineImage.setImageResource(R.drawable.line_blue_non_bottom);
-                holder.oimgName.setText("last");
-            }
-        }
-        else
-        {
-            holder.dateLinear.setVisibility(View.VISIBLE);
-            holder.etc.setVisibility(View.GONE);
-        }
 
-        holder.oTextTitle.setText(m_oData.get(position).strTitle);
-        holder.oTextTitle.setTag(position);
-        holder.oTextTIme.setText(m_oData.get(position).strTIme);
-        holder.oTextDate.setText(m_oData.get(position).strDate);
-        holder.oTextLat.setText(m_oData.get(position).strLat);
-        holder.oTextLng.setText(m_oData.get(position).strLng);
-        dateSave = m_oData.get(position).strDate;
-
-        return convertView;
+        return v;
     }
 
-    public class CustomViewHolder
+    static class CustomViewHolder
     {
         TextView oTextTitle;
         TextView oTextTIme;
